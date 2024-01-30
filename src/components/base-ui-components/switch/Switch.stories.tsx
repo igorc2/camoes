4d39055967225956a2fdfcc411661b7e2391fc5f
igor coelho <igorc2@gmail.com>
2024-01-30 16:01:14 +0000

    test

--- src/components/base-ui-components/switch/Switch.stories.tsx
+++ src/components/base-ui-components/switch/Switch.stories.tsx
@@ -6,14 +6,14 @@ import Switch from './Switch';
 
 
 const meta = {
-  title: 'Example/MUI Base UI Switch',
+  title: "Example/MUI Base UI Switch coiso",
   component: Switch,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     // backgroundColor: { control: 'color' },
